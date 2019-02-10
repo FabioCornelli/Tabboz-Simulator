c5dfd9d7dc03694d059b736eddbd7271e241f335
Antonio Willy Malara <antonio.malara@gmail.com>
2019-02-10 15:49:06 +0100

    Define BOOL, argv, and undefine calling conventions

--- Tabboz Simulator/C/os.h
+++ Tabboz Simulator/C/os.h
@@ -1,1 +1,15 @@
+char * _argv[] = {""};
+
+#define FAR
+#define NEAR
+#define PASCAL
+
+#define TABBOZ_WIN32
+
+typedef _Bool BOOL;
+static _Bool FALSE = 0;
+static _Bool TRUE = 1;
+
+#define random tabboz_random
+#define openlog tabboz_openlog
 
