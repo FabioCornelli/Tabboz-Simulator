9d51feec6f1e615a9895bc8c62e5cf6420fc0ab5
Antonio Willy Malara <antonio.malara@gmail.com>
2019-02-10 03:25:05 +0100

    Adding Xcode project

---
+++ Tabboz Simulator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Tabboz Simulator
+//
+//  Created by Antonio Malara on 10/02/2019.
+//  Copyright © 2019 Antonio Malara. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
